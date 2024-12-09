a8fc77d5068fcbaff14aa916cb88166bda21f0e0
vsheo <vireshsheoratan@gmail.com>
2024-12-09 10:58:03 +0100

    hover flip #1
    de eerste event is af

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,17 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+
+// Frontend interaction
+let upSideDown = document.querySelector('a:nth-of-type(1)')
+
+upSideDown.addEventListener('mouseover', flipHandler)
+
+function flipHandler() {
+  upSideDown.classList.toggle('flip180deg')
+}
+
+
+// click rotate
+let rotate2Times = document.querySelector('a:nth-of-type(2)')
